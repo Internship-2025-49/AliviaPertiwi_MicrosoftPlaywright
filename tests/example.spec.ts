81d4f67d89ebea23fc975cff18fee67d619216da
aliviaaaa <lil.alivia23@gmail.com>
2025-01-30 17:02:04 +0700

    test: Configure Projects #6

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,30 +1,19 @@
 import { test, expect } from "@playwright/test";
-test("active and completed filters", async ({ page }) => {
-  await page.goto("https://demo.playwright.dev/todomvc/#/");
-  await page
-    .getByPlaceholder("What needs to be done?")
-    .fill("water the plants");
-  await page.getByPlaceholder("What needs to be done?").press("Enter");
-  await page.getByPlaceholder("What needs to be done?").fill("feed the dog");
-  await page.getByPlaceholder("What needs to be done?").press("Enter");
-  await page
-    .locator("li")
-    .filter({ hasText: "water the plants" })
-    .getByLabel("Toggle Todo")
-    .check();
-  await page.getByRole("link", { name: "Active" }).click();
-  await expect(page.getByTestId("todo-title")).toContainText("feed the dog");
-  await page.getByRole("link", { name: "Completed" }).click();
-  await expect(page.getByTestId("todo-title")).toContainText(
-    "water the plants"
-  );
+test("has title", async ({ page }) => {
+  await page.goto("https://playwright.dev/");
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Playwright/);
 });
 
-test("text field is cleared when item is added", async ({ page }) => {
-  await page.goto("https://demo.playwright.dev/todomvc/#/");
-  await page
-    .getByPlaceholder("What needs to be done?")
-    .fill("water the plants");
-  await page.getByPlaceholder("What needs to be done?").press("Enter");
-  await expect(page.getByPlaceholder("What needs to be done?")).toBeEmpty();
+test("get started link", async ({ page }) => {
+  await page.goto("https://playwright.dev/");
+
+  // Click the get started link.
+  await page.getByRole("link", { name: "Get started" }).click();
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(
+    page.getByRole("heading", { name: "Installation" })
+  ).toBeVisible();
 });
