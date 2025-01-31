a305eab1e567428c8dc3c004ec0501e42bfb502d
aliviaaaa <lil.alivia23@gmail.com>
2025-01-31 13:23:52 +0700

    test: Use BaseURL in test spec

--- playwright.config.ts
+++ playwright.config.ts
@@ -26,7 +26,6 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: "https://demo.playwright.dev/todomvc/#/",
     trace: "on-first-retry",
     // trace: "on",
     // screenshot: "on",
