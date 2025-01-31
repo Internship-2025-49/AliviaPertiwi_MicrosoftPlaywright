d1bcf39110c4de7b3cd6efed2292a559c481c7e8
aliviaaaa <lil.alivia23@gmail.com>
2025-01-31 13:31:44 +0700

    test: Create a reusable locator in test spec #1

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,20 +1,32 @@
-import { test, expect, Page } from "@playwright/test";
+import { test, expect } from "@playwright/test";
+test.describe("New Todo", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto("");
+  });
 
-test("has title", async ({ page }) => {
-  await page.goto("https://playwright.dev/");
+  test("active and completed filters", async ({ page }) => {
+    const todoInput = page.getByPlaceholder("What needs to be done?");
+    await todoInput.fill("water the plants");
+    await todoInput.press("Enter");
+    await todoInput.fill("feed the dog");
+    await todoInput.press("Enter");
+    await page
+      .locator("li")
+      .filter({ hasText: "water the plants" })
+      .getByLabel("Toggle Todo")
+      .check();
+    await page.getByRole("link", { name: "Active" }).click();
+    await expect(page.getByTestId("todo-title")).toContainText("feed the dog");
+    await page.getByRole("link", { name: "Completed" }).click();
+    await expect(page.getByTestId("todo-title")).toContainText(
+      "water the plants"
+    );
+  });
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
-
-test("get started link", async ({ page }) => {
-  await page.goto("https://playwright.dev/");
-
-  // Click the get started link.
-  await page.getByRole("link", { name: "Get started" }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(
-    page.getByRole("heading", { name: "Installation" })
-  ).toBeVisible();
+  test("text field is cleared when item is added", async ({ page }) => {
+    const todoInput = page.getByPlaceholder("What needs to be done?");
+    await todoInput.fill("water the plants");
+    await todoInput.press("Enter");
+    await expect(page.getByPlaceholder("What needs to be done?")).toBeEmpty();
+  });
 });
